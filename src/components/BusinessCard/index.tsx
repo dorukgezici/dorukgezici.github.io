ba0ca4994c7d7440adbd5ca2581e56177d12716c
Doruk Gezici <doruk@gezici.me>
2023-07-10 02:41:19 +0200

    More responsivity issues handled

--- src/components/BusinessCard/index.tsx
+++ src/components/BusinessCard/index.tsx
@@ -15,9 +15,9 @@ import styles from './BusinessCard.module.scss';
 
 export default function BusinessCard() {
   return (
-    <div className={classNames(styles.card, 'w-11/12', 'sm:w-10/12', 'md:w-9/12', 'lg:w-8/12', 'xl:w-[800px]')}>
+    <div className={classNames(styles.card, 'w-11/12 h-2/3', 'sm:w-10/12', 'md:w-9/12', 'lg:w-8/12', 'xl:w-[800px]')}>
       <h1 className={styles.name}>Doruk Gezici</h1>
-      <h2 className={styles.title}>Full Stack Developer &amp; Entrepreneur</h2>
+      <h2 className={classNames(styles.title, 'my-5')}>Full Stack Developer &amp; Entrepreneur</h2>
       <div className={styles.about}>
         <p>
           <a href="/">DGtech</a>,&nbsp;
@@ -25,29 +25,31 @@ export default function BusinessCard() {
           <a href="https://gamerarena.com" target="_blank">Gamer Arena</a>,&nbsp;
           <a href="https://ituscheduler.com" target="_blank">ITUscheduler</a>
         </p>
-        <a href="mailto:doruk@gezici.me" className="c2a-btn mt-10 mr-5" target="_blank">
-          <FontAwesomeIcon icon={faEnvelope} className="mr-2"/>Contact Me
-        </a>
-        <a href="https://calendly.com/dorukgezici/30min" className="c2a-btn mt-10" target="_blank">
-          <FontAwesomeIcon icon={faCalendar} className="mr-2"/>Book a Meeting
-        </a>
+        <div className="flex flex-wrap justify-center gap-5 mt-10">
+          <a href="mailto:doruk@gezici.me" className="c2a-btn" target="_blank">
+            <FontAwesomeIcon icon={faEnvelope} className="mr-2"/>Contact Me
+          </a>
+          <a href="https://calendly.com/dorukgezici/30min" className="c2a-btn" target="_blank">
+            <FontAwesomeIcon icon={faCalendar} className="mr-2"/>Book a Meeting
+          </a>
+        </div>
       </div>
 
-      <div className={styles.social}>
+      <div className={classNames(styles.social, 'flex', 'flex-wrap', 'gap-4', 'sm:gap-8', 'md:gap-12', 'lg:gap-14')}>
         <a href="https://linkedin.com/in/dorukgezici" target="_blank">
-          <FontAwesomeIcon icon={faLinkedinIn} className={classNames(styles.icon, 'mr-14')}/>
+          <FontAwesomeIcon icon={faLinkedinIn} className={styles.icon}/>
         </a>
         <a href="https://dorukgezici.medium.com" target="_blank">
-          <FontAwesomeIcon icon={faMediumM} className={classNames(styles.icon, 'mr-14')}/>
+          <FontAwesomeIcon icon={faMediumM} className={styles.icon}/>
         </a>
         <a href="https://github.com/dorukgezici" target="_blank">
-          <FontAwesomeIcon icon={faGithub} className={classNames(styles.icon, 'mr-14')}/>
+          <FontAwesomeIcon icon={faGithub} className={styles.icon}/>
         </a>
         <a href="https://instagram.com/dorukgezici" target="_blank">
-          <FontAwesomeIcon icon={faInstagram} className={classNames(styles.icon, 'mr-14')}/>
+          <FontAwesomeIcon icon={faInstagram} className={styles.icon}/>
         </a>
         <a href="https://twitter.com/derektraveller" target="_blank">
-          <FontAwesomeIcon icon={faTwitter} className={classNames(styles.icon, 'mr-14')}/>
+          <FontAwesomeIcon icon={faTwitter} className={styles.icon}/>
         </a>
         <a href="https://facebook.com/dorukgezici" target="_blank">
           <FontAwesomeIcon icon={faFacebookF} className={styles.icon}/>
