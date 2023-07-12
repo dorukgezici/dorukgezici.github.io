d534255157a7d4e382c13b7524f2d43ad3f99de5
Doruk Gezici <doruk@gezici.me>
2023-07-12 16:11:16 +0200

    Added Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Lato } from 'next/font/google';
 import classNames from 'classnames';
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -21,6 +22,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+    {/* Google Tag Manager */}
+    {process.env.NODE_ENV !== 'development' && (
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5J976B');
+          `,
+        }}
+      />
+    )}
     <body className={classNames(font.className, 'text-neutral-400 bg-neutral-900')}>
     {children}
     <footer className="flex justify-center my-10">© 2023 | DGtech</footer>
