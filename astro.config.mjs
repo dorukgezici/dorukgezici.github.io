c4298dda5b23e2b2d089e0af802d95a6e13a7955
Doruk Gezici <doruk@gezici.me>
2023-08-22 14:35:14 +0200

    Added site url for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://doruk.gezici.me",
   integrations: [tailwind(), react(), sitemap()],
   output: "server",
   adapter: vercel(),
