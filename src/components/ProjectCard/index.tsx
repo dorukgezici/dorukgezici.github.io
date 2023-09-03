2397a40357c5e1ddb3898ef1611fc9544d3a846a
Doruk Gezici <doruk@gezici.me>
2023-09-04 01:05:16 +0200

    Added projects section

---
+++ src/components/ProjectCard/index.tsx
@@ -0,0 +1,48 @@
+import { faCode, faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+type Props = {
+  imgUrl: string;
+  title: string;
+  description: string;
+  gitUrl: string;
+  previewUrl: string;
+};
+
+export default function ProjectCard({ imgUrl, title, description, gitUrl, previewUrl }: Props) {
+  return (
+    <div>
+      <div
+        className="min-w-[375px] h-52 md:h-72 rounded-t-xl relative group"
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+          <a
+            href={gitUrl}
+            target={gitUrl.startsWith("#") ? "_self" : "_blank"}
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+          >
+            <FontAwesomeIcon
+              icon={faCode}
+              className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white"
+            />
+          </a>
+          <a
+            href={previewUrl}
+            target={previewUrl.startsWith("#") ? "_self" : "_blank"}
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+          >
+            <FontAwesomeIcon
+              icon={faEye}
+              className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ProjectsSection/index.tsx
@@ -0,0 +1,108 @@
+import ProjectCard from "@/components/ProjectCard";
+import ProjectTag from "@/components/ProjectTag";
+import { motion, useInView } from "framer-motion";
+import { useRef, useState } from "react";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Subabot",
+    description: "Slack bot to track keywords on RSS feeds",
+    image: "/img/projects/subabot.webp",
+    tag: ["All", "Bot", "Web"],
+    gitUrl: "https://github.com/dorukgezici/subabot",
+    previewUrl: "https://subabot.gezici.me",
+  },
+  {
+    id: 2,
+    title: "CryptoPatro",
+    description: "Personal cryptoasset portfolio tracker and trading assistant bot",
+    image: "/img/projects/cryptopatro.webp",
+    tag: ["All", "Bot", "Web"],
+    gitUrl: "https://github.com/dorukgezici/CryptoPatro",
+    previewUrl: "#projects",
+  },
+  {
+    id: 3,
+    title: "Yeşil Mavi Hayat",
+    description: "A book promotion website",
+    image: "/img/projects/yesilmavihayat.webp",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/dorukgezici/yesilmavihayat",
+    previewUrl: "https://yesilmavihayat.com",
+  },
+  {
+    id: 4,
+    title: "ITUscheduler",
+    description: "Istanbul Technical University course crawler & scheduler",
+    image: "/img/projects/ituscheduler.webp",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/dorukgezici/ituscheduler-go",
+    previewUrl: "https://ituscheduler.com",
+  },
+  {
+    id: 5,
+    title: "Gamer Arena",
+    description: "Competitive Esports Platform",
+    image: "/img/projects/gamerarena.webp",
+    tag: ["All", "Startup"],
+    gitUrl: "#projects",
+    previewUrl: "https://gamerarena.com",
+  },
+  {
+    id: 6,
+    title: "Astro DecapCMS OAuth Integration",
+    description: "Astro integration for the Decap CMS with custom OAuth backend",
+    image: "/img/projects/astro-decapcms-oauth.webp",
+    tag: ["All", "Library"],
+    gitUrl: "https://github.com/dorukgezici/astro-decap-cms-oauth",
+    previewUrl: "https://astro-decap-cms-oauth.vercel.app",
+  },
+];
+
+export default function ProjectsSection() {
+  const [tag, setTag] = useState<string>("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag: string) => setTag(newTag);
+  const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects" className="flex flex-col justify-center items-center gap-8">
+      <h2 className="text-center text-6xl font-bold text-white mt-4 mb-8">Projects</h2>
+      <div className="text-white flex flex-wrap justify-center items-center gap-2 py-6">
+        <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
+        <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
+        <ProjectTag onClick={handleTagChange} name="Bot" isSelected={tag === "Bot"} />
+        <ProjectTag onClick={handleTagChange} name="Startup" isSelected={tag === "Startup"} />
+        <ProjectTag onClick={handleTagChange} name="Library" isSelected={tag === "Library"} />
+      </div>
+      <ul ref={ref} className="grid lg:grid-cols-2 xl:grid-cols-3 gap-8 xl:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+}
